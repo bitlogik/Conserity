ebe81c365e57a5d0dae7f9aab22ed4eeb8264fc8
Antoine FERRON <antoine.ferron@bitlogik.fr>
2020-01-15 00:33:46 +0100

    Allow port 80

--- run-conserity.sh
+++ run-conserity.sh
@@ -230,6 +230,7 @@ ufw default allow outgoing >> $conserity_log_file
 ufw allow $SSHPORT/tcp >> $conserity_log_file
 ufw limit $SSHPORT/tcp >> $conserity_log_file
 ufw allow 443/tcp >> $conserity_log_file
+ufw allow 80/tcp >> $conserity_log_file
 ufw deny 68 >> $conserity_log_file
 ufw deny 5100 >> $conserity_log_file
 ufw allow 53 >> $conserity_log_file
